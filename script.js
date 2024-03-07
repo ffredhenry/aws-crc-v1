b438ffd058a47be3b60d5c8bce2f59d574479f13
ffredhenry <f.fredhenry@gmail.com>
2024-03-07 16:10:12 -0600

    decided against additional obfuscating steps at this time

--- script.js
+++ script.js
@@ -5,13 +5,19 @@ document.addEventListener('DOMContentLoaded', function()
 	const employmentHistory = [
 		{
 			id: 1, 
-			title: "Security Analyst", 
+			title: "Network Security Engineer", 
 			company: "AT&T Cybersecurity", 
 			geolocation: "Austin, TX",
 			years: "March 2023 - Present", 
-			description: 
+			description: //using HTML/CSS elements directly within template literals to bypass styling
 			`
-			param_job_1
+			Super SOC Analyst.<br>
+			● Investigate alerts in customer Azure and AWS environments to discover potential security misconfigurations<br>
+			● Monitor and take action on customer endpoints using SentinelOne EDR to proactively block and mitigate threats<br>
+			● Identify excess false positive alerts and document for SOAR team to implement filtering rules in AlienVault SIEM<br>
+			● Leverage sandboxed VMs and open-source intelligence tools to investigate URLs and files from phishing emails<br>
+			● Analyze firewall logs from Cisco, Fortinet, and Palo Alto appliances to uncover C2 endpoint beaconing activity<br>
+			● Produce and deliver reports to customers on compliance with NIST, ISO 27001, HIPAA and PCI DSS standards<br>
 			`
 		},
 		{
@@ -22,7 +28,12 @@ document.addEventListener('DOMContentLoaded', function()
 			years: "October 2020 - February 2023",
 			description:
 			`
-			param_job_2
+			QA specialist for Meta's products suites: Facebook (Social Impact), Data Engineering (daiquery, unidash, scuba), and Reality Labs (Oculus and Horizon Worlds).<br>
+			● Resolved over 4000 incidents over the course of 2 years with a customer satisfaction rate exceeding 99%<br>
+			● Performed static and dynamic analysis of backend SQL queries to troubleshoot data visualization dashboards<br>
+			● Scheduled and led training sessions for six new employees over ten months on products, triage process and tools<br>
+			● Collaborated with cross-functional teams to identify issues and potential resolutions in new software features<br>
+			● Eliminated 1500 billable hours of developer time by performing software bisects to locate root causes of issues<br>
 			`
 		},
 		{
@@ -33,7 +44,11 @@ document.addEventListener('DOMContentLoaded', function()
 			years: "June 2019 - April 2020",
 			description:
 			`
-			param_job_3
+			Rotational program targeting Sales, Operations, and Supply Chain Management.<br>
+			● Reduced processing time for external inventory data by 5 hours using Excel macros to format for upload to SAP<br>
+			● Developed and revised new and existing training material for company SAP installation and custom transactions<br>
+			● Removed 300 discontinued items from product catalog, reducing backlog on materials management by 40 hours<br>
+			● Designed testing framework to evaluate accuracy of prototype material search tool against 3000 customer orders<br>
 			`
 		},
 		{
@@ -44,7 +59,11 @@ document.addEventListener('DOMContentLoaded', function()
 			years: "August 2018 - May 2019",
 			description:
 			`
-			param_job_4
+			Founding member of the Zachry Engineering IT Department<br>
+			● Significantly reduced the occurrence of wireless microphone outages through implementing remote monitoring<br>
+			● Trained 20 faculty members on usage of digital whiteboard technology and lecture recording in classrooms<br>
+			● Created documentation for 70 new hires on setup of ServiceNow dashboard for incident monitoring and triage<br>
+			● Regularly went the extra mile in resolving faculty and student IT issues by working outside of allotted time slots<br>
 			`
 		},
 		{
@@ -55,12 +74,16 @@ document.addEventListener('DOMContentLoaded', function()
 			years: "May 2018 - August 2018",
 			description:
 			`
-			param_job_5
+			Digital Transformation specialist<br>
+			● Increased order throughput in warehouse with barcoding and ERP integration to allow real-time inventory control<br>
+			● Transformed HR to paperless environment with web-based applicant tracking, payroll and vacation management<br>
+			● Streamlined order fulfillment processes for a high-volume account by adding 400 new product lines in inventory<br>
+			● Executed remarketing campaign for salvageable IT assets to fund e-waste recycling of deprecated infrastructure<br>
 			`
 		}
 	];
 
-	// Technical Skills Drop-down
+	///// FIXME: Add a 'const' for Skills
 	const technicalSkills = [
 		{
 			id: 1,
@@ -78,7 +101,6 @@ document.addEventListener('DOMContentLoaded', function()
 			skills: "Python, SQL, Bash, Javascript, CSS, HTML, Git/GitHub, AWS-CLI"
 		}
 	];
-	
 	
 	// Timeline declaration and entries for employment history
 	const exp_timeline = document.getElementById('exp_timeline');
